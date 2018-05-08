017de5831c87354dda48be1fceb3691ca8d3d456
NightNineNeighbor <dud9203@gmail.com>
2018-05-08 16:13:56 +0900

    파일 업로드
    라이브러리를 이요한다.

--- BoardWeb/src/main/java/com/springbook/view/board/BoardController.java
+++ BoardWeb/src/main/java/com/springbook/view/board/BoardController.java
@@ -1,11 +1,15 @@
 package com.springbook.view.board;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.board.impl.BoardService;
@@ -18,7 +22,14 @@ public class BoardController {
 	
 	//글 등록
 	@RequestMapping("/insertBoard.do")
-	public String insertBoard(BoardVO vo) {
+	public String insertBoard(BoardVO vo) throws IOException {
+		//파일 업로드 처리
+		MultipartFile uploadFile = vo.getUploadFile();
+		if(!uploadFile.isEmpty()) {
+			String fileName = uploadFile.getOriginalFilename();
+			uploadFile.transferTo(new File("D:/" + fileName));
+		}
+		
 		boardService.insertBoard(vo);
 		return "getBoardList.do";
 	}
