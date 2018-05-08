fb42ad17a3a0b20d08ef6a8710e0fd9c63c007ff
NightNineNeighbor <dud9203@gmail.com>
2018-05-08 15:34:04 +0900

    annotation구현
    이전 코드를 어노테이션을 이용해 구현.

---
+++ BoardWeb/src/main/java/com/springbook/view/board/BoardController.java
@@ -0,0 +1,53 @@
+package com.springbook.view.board;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.springbook.biz.board.BoardVO;
+import com.springbook.biz.board.impl.BoardService;
+
+@Controller
+@SessionAttributes("board")
+public class BoardController {
+	@Autowired
+	private BoardService boardService;
+	
+	//글 등록
+	@RequestMapping("/insertBoard.do")
+	public String insertBoard(BoardVO vo) {
+		boardService.insertBoard(vo);
+		return "getBoardList.do";
+	}
+	
+	//글 수정
+	@RequestMapping("/updateBoard.do")
+	public String updateBoard(BoardVO vo) {
+		boardService.updateBoard(vo);
+		return "getBoardList.do";
+	}
+	
+	//글 삭제
+	@RequestMapping("/deleteBoard.do")
+	public String deleteBoard(@ModelAttribute("board") BoardVO vo) {
+		boardService.deleteBoard(vo);
+		return "getBoardList.do";
+	}
+	
+	//글 상세 조회
+	@RequestMapping("/getBoard.do")
+	public String getBoard(BoardVO vo, Model model) {
+		model.addAttribute("board", boardService.getBoard(vo));
+		return "getBoard.jsp";
+	}
+	
+	//글 목록 검색
+	@RequestMapping("/getBoardList.do")
+	public String getBoardList(BoardVO vo, Model model) {
+		model.addAttribute("boardList", boardService.getBoardList(vo));
+		return "getBoardList.jsp";
+	}
+}
